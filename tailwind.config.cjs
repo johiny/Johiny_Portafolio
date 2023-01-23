cfccfd579f1a1bf3d882715f21bf88e230292691
Johiny <johiny7@live.com>
2023-01-23 18:34:50 -0500

    first commit!

---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		colors: {
+			"xiketic" : "#0D0221",
+			"midnightBlue" : "#0F084B",
+			"darkCornflowerBlue": "#0F084B",
+			"lightBlue" : "#A6CFD5",
+			"aeroBlue" : "#C2E7D9"
+		},
+		extend: {},
+	},
+	plugins: [],
+}
