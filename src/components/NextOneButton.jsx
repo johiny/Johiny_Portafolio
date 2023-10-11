a898e9a9e39dcfe12ae93ea3563c72254e158ad0
Johiny <johiny7@live.com>
2023-10-10 22:17:14 -0500

    boton next one funcionando

---
+++ src/components/NextOneButton.jsx
@@ -0,0 +1,24 @@
+import "./NextOneButton.css"
+
+function findClosestSection(targetElement) {
+    const xpath = `following-sibling::section[1] | preceding-sibling::section[1]`;
+    const result = document.evaluate(xpath, targetElement, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+    return result.singleNodeValue;
+  }
+
+const NextOneButton = () => {
+  return (
+    <div className="flex flex-col absolute top-10 right-[33%] animate-pulse cursor-pointer hover:opacity-70" onClick={(e) => {
+        const nextSection = findClosestSection(e.target.parentNode.parentNode.parentNode);
+        nextSection.scrollIntoView({
+            behavior: "smooth",
+            block: 'center',
+            inline: 'center'
+        })
+    }}>
+    <h2 className="text-center">Go to next one<br/>▼</h2>
+    </div>
+  )
+}
+
+export default NextOneButton
